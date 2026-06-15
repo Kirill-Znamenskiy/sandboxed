187e06ed158464cbc159319c81cc98814b7e44b7
Kirill.Znamenskiy.pw <Kirill@Znamenskiy.pw>
2026-06-15 00:24:31 +0000

    Use generic Python dependency

--- Formula/sandboxed.rb
+++ Formula/sandboxed.rb
@@ -8,17 +8,17 @@ class Sandboxed < Formula
   sha256 "d25d5a6532c021958d46255c60ea1a2c5a20df94e485df029938c65dd76ea548"
   license "MIT"
 
-  depends_on "python@3.13"
+  depends_on "python"
 
   resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
-    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
   end
 
   def install
     libexec.install "src", "targets"
 
-    venv = virtualenv_create(libexec/"venv", "python3.13")
+    venv = virtualenv_create(libexec/"venv", "python3")
     venv.pip_install resource("pyyaml")
 
     ["sandboxed", "sbxd"].each do |command_name|
